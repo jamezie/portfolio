87af484d767371c65cbba3f100cec388416c0b70
James K <jame_zie@yahoo.com>
2016-07-18 05:46:19 -0700

    bare nav up. Use Zurb foundation, rid of semantic UI

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -1,6 +1,31 @@
-var portfolioApp = angular.module('portfolioApp', []);
+var portfolioApp = angular.module('portfolioApp', ['ngRoute']);
+
+portfolioApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'home.html',
+			controller: 'HomeCtrl'
+		})
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'AuthenticateCtrl'
+		})
+		.when('/signup', {
+			templateUrl: 'signup.html',
+			controller: 'AuthenticateCtrl'
+		});
+});
 
 portfolioApp.controller('BodyCtrl', function ($scope) {
 	$scope.title = "James Portfolio";
+	return $scope;
+});
+
+portfolioApp.controller('AuthenticateCtrl', function($scope) {
+	$scope.error_message="";
+	return $scope;
+});
+
+portfolioApp.controller('HomeCtrl', function($scope) {
 	return $scope;
 });
